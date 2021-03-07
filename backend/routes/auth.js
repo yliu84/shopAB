801b1e56a54867af83a2c446541ee83be55dd363
yliu84 <yliu84@studentmail.nait.ca>
2021-03-07 16:37:02 -0700

    Initial Commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    registerUser, 
+    loginUser, 
+    forgotPassword, 
+    resetPassword,
+    getUserProfile,
+    updatePassword,
+    updateProfile,
+    allUsers,
+    getUserDetails,
+    updateUser,
+    deleteUser,
+    logout 
+} = require('../controllers/authController');
+
+const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
+
+router.route('/register').post(registerUser);
+router.route('/login').post(loginUser);
+router.route('/password/forgot').post(forgotPassword);
+router.route('/password/reset/:token').put(resetPassword);
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/logout').get(logout);
+
+// Admin routes
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id')
+    .get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails)
+    .put(isAuthenticatedUser, authorizeRoles('admin'), updateUser)
+    .delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
+
+
+module.exports = router;
---
+++ backend/routes/product.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+        getProducts,
+        getAdminProducts, 
+        newProduct, 
+        getSingleProduct, 
+        updateProduct, 
+        deleteProduct,
+        createProductReview,
+        getProductReviews,
+        deleteReview 
+    } = require('../controllers/productController');
+
+const {isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
+
+router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
+
+router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateProduct);
+router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+
+router.route('/review').put(isAuthenticatedUser, createProductReview);
+router.route('/reviews').get(isAuthenticatedUser, getProductReviews);
+router.route('/reviews').delete(isAuthenticatedUser, deleteReview);
+
+
+module.exports = router;
