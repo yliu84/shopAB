0ffe7407b7fc7d39c8127c08e82c3d3fa6db1491
yliu84 <yliu84@studentmail.nait.ca>
2021-03-08 11:01:54 -0700

    add frontend folder back

---
+++ frontend/src/store.js
@@ -0,0 +1,65 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+import {
+        productsReducer, 
+        productDetailsReducer,
+        productReducer,
+        newProductReducer, 
+        newReviewReducer,
+        productReviewsReducer,
+        reviewReducer
+    } from './reducers/productReducers';
+import {
+        authReducer, 
+        userReducer, 
+        forgotPasswordReducer, 
+        allUsersReducer,
+        userDetailsReducer
+    } from './reducers/userReducers';
+import {cartReducer} from './reducers/cartReducers';
+import {
+        newOrderReducer, 
+        myOrdersReducer, 
+        orderDetailsReducer, 
+        allOrdersReducer, 
+        orderReducer
+    } from './reducers/orderReducers';
+
+const reducer = combineReducers({
+    products: productsReducer,
+    newProduct: newProductReducer,
+    productDetails: productDetailsReducer,
+    product: productReducer,
+    newReview: newReviewReducer,
+    productReviews: productReviewsReducer,
+    review: reviewReducer,
+    auth: authReducer,
+    user: userReducer,
+    allUsers: allUsersReducer,
+    userDetails: userDetailsReducer,
+    forgotPassword: forgotPasswordReducer,
+    cart: cartReducer,
+    newOrder: newOrderReducer,
+    myOrders: myOrdersReducer,
+    orderDetails: orderDetailsReducer,
+    allOrders: allOrdersReducer,
+    order: orderReducer
+});
+
+let initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems') 
+            ? JSON.parse(localStorage.getItem('cartItems')) 
+            : [],
+        shippingInfo: localStorage.getItem('shippingInfo')
+            ? JSON.parse(localStorage.getItem('shippingInfo'))
+            : {}
+    }
+};
+
+const middleware = [thunk];
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
